cfaf7e05ae94f3c33877c2c9f8bfe78c5012bfd5
maddyb99 <43785086+maddyb99@users.noreply.github.com>
2021-03-14 19:32:41 +0530

    switch to swagger-stats

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,12 @@ import * as dotenv from "dotenv";
 // import mongoose from 'mongoose';
 import helmet from "helmet";
 // import { apiInternal as apiV1 } from "./v1/main";
+// import promBundle from "express-prom-bundle";
+const swStats = require("swagger-stats");
+// const apiSpec = require("swagger.json");
 
 dotenv.config();
+// const metricsMiddleware = promBundle({ includeMethod: true });
 
 if (!process.env.PORT) {
   console.log(`Error to get ports`);
@@ -19,6 +23,8 @@ if (!process.env.PORT) {
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+// app.use(metricsMiddleware);
+app.use(swStats.getMiddleware({  }));
 
 const server = app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
